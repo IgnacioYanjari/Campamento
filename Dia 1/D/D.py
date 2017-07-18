2043f6b37f25e88145bcbbaa8ba3f958bae04112
Ignacio Yanjari <nach116@hotmail.com>
2017-07-17 22:46:30 -0400

    Fix

---
+++ Dia 1/D/D.py
@@ -0,0 +1,5 @@
+n=int(input())
+if n%2==0:
+	print("2\n")
+else:
+	print ("1\n")
--- Dia 1/E/E.cpp
+++ Dia 1/E/E.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-int main(){
-	int A, B;
+long long main(){
+	long long A, B;
 	size_t cnt = 0;
 
-	std::vector<int> black;
-	std::vector<int> white;
+	std::vector<long long> black;
+	std::vector<long long> white;
 
 	string str;
 	cin >> A >> B;
@@ -21,19 +21,19 @@ int main(){
 			cnt++;
 	}
 
-	for (int ix = cnt - 1; ix >= 0; ix--){
+	for (long long ix = cnt - 1; ix >= 0; ix--){
 		if(str[ix] == 'W')
 			white.push_back(ix);
 	}
 
-	for (int ix = cnt; ix < str.size(); ix++){
+	for (long long ix = cnt; ix < str.size(); ix++){
 		if(str[ix] == 'B')
 			black.push_back(ix);
 	}
-	int sum=0;
-	int C1 = A;
-	int C2 = A - B;
-	for (int ix=0 ; ix < cnt;ix++){
+	long long sum=0;
+	long long C1 = A;
+	long long C2 = A - B;
+	for (long long ix=0 ; ix < cnt;ix++){
 		sum = sum + min( abs(black[ix]-white[ix]) * C2,C1);
 	}
 	cout<<sum<<"\n";
