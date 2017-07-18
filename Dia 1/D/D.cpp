f24de40cefc16af56521264198c8974a52d10617
Ignacio Yanjari <nach116@hotmail.com>
2017-07-17 22:40:35 -0400

    Dia(1) -1

---
+++ Dia 1/D/D.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <cinttypes>
+
+using namespace std;
+
+int main(){
+	long long A, B;
+	long long cnt = 0;
+
+	std::vector<long long> black;
+	std::vector<long long> white;
+
+	string str;
+	cin >> A >> B;
+	cin >> str;
+
+	for (auto &letter : str){
+		if (letter == 'B')
+			cnt++;
+	}
+
+	for (long long ix = cnt - 1; ix >= 0; ix--){
+		if(str[ix] == 'W')
+			white.push_back(ix);
+	}
+
+	for (long long ix = cnt; ix < str.size(); ix++){
+		if(str[ix] == 'B')
+			black.push_back(ix);
+	}
+	long long sum=0;
+	long long C1 = A;
+	long long C2 = A - B;
+	for (long long ix=0 ; ix < cnt;ix++){
+		sum = sum + min( abs(black[ix]-white[ix]) * C2,C1);
+	}
+	cout<<sum<<"\n";
+
+	return 0;
+}
